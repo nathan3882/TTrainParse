da56610a5d58c4653a9f4693f8a41ab0b1967de8
nathan3882 <nathan.allanson14@hotmail.com>
2018-12-19 12:25:59 +0000

    close connection task + error handling + newCon() in constructor

--- src/main/java/me/nathan3882/data/SqlConnection.java
+++ src/main/java/me/nathan3882/data/SqlConnection.java
@@ -1,10 +1,13 @@
 package me.nathan3882.data;
 
 import me.nathan3882.ttrainparse.TTrainParser;
+import me.nathan3882.ttrainparse.TaskManager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 public class SqlConnection {
 
@@ -12,27 +15,7 @@ public class SqlConnection {
     private boolean open;
 
     private static long latestOpeningMilis;
-
-    public static void setLatestOpeningMilis(long latestOpeningMilis) {
-        SqlConnection.latestOpeningMilis = latestOpeningMilis;
-    }
-
-    public TTrainParser getTTrainParser() {
-        return this.main;
-    }
-
-    public boolean isClosed() {
-        try {
-            return this.connection.isClosed();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public boolean isOpen() {
-        return !isClosed();
-    }
+    private TaskManager closeConnectionTask;
 
     public interface SqlTableName {
         String TIMETABLE_RENEWAL = "timetablerenewal";
@@ -50,6 +33,7 @@ public class SqlConnection {
 
     public SqlConnection(TTrainParser main) {
         this.main = main;
+        this.connection = newCon();
     }
 
     public SqlConnection(TTrainParser main, String host, int port, String databaseName, String username, String password) {
@@ -67,14 +51,44 @@ public class SqlConnection {
      * @return success or not
      */
     public void openConnection() {
+        if (!connectionEstablished()) {
+            newCon();
+        }
+        if (connectionEstablished()) {
+            if (closeConnectionTask == null) {
+                closeConnectionTask = new TaskManager(new Timer()) {
+                    @Override
+                    public void run() {
+                        long current = System.currentTimeMillis();
+                        long lastOpen = SqlConnection.getLatestOpeningMilis();
+                        if (connectionEstablished() && isOpen() &&
+                                current - lastOpen >= TimeUnit.SECONDS.toMillis(150)) { //Close after 2 and 1/2 minutes
+                            closeConnection();
+                            SqlConnection.setLatestOpeningMilis(current); //Will be 0, condition will fail unless has been opened again
+                        }
+                    }
+                };
+                closeConnectionTask.runTaskSynchronously(closeConnectionTask, 5000L, 5000L);
+            }
+            try {
+                latestOpeningMilis = System.currentTimeMillis();
+                if (open) return;
+                Class.forName("com.mysql.jdbc.Driver");
+                this.connection = newCon();
+                open = true;
+            } catch (Exception e) {
+                TTrainParser.getDebugManager().handle(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Connection newCon() {
         try {
-            latestOpeningMilis = System.currentTimeMillis();
-            if (open) return;
-            Class.forName("com.mysql.jdbc.Driver");
-            this.connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?allowMultiQueries=true", username, password);
-            open = true;
+            return DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?allowMultiQueries=true", username, password);
         } catch (Exception e) {
-            e.printStackTrace();
+            TTrainParser.getDebugManager().handle(e, "Could not connect to SQL Database, are the details correct?");
+            return null;
         }
     }
 
@@ -85,6 +99,10 @@ public class SqlConnection {
         }
     }
 
+    public boolean connectionEstablished() {
+        return this.connection != null;
+    }
+
     public Connection getConnection() {
         return connection;
     }
@@ -93,8 +111,32 @@ public class SqlConnection {
         try {
             resource.close();
         } catch (Exception e) {
+            TTrainParser.getDebugManager().handle(e);
             e.printStackTrace();
         }
+    }
+
+
+    public static void setLatestOpeningMilis(long latestOpeningMilis) {
+        SqlConnection.latestOpeningMilis = latestOpeningMilis;
+    }
+
+    public TTrainParser getTTrainParser() {
+        return this.main;
+    }
+
+    public boolean isClosed() {
+        try {
+            return this.connection.isClosed();
+        } catch (SQLException e) {
+            TTrainParser.getDebugManager().handle(e);
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean isOpen() {
+        return !isClosed();
     }
 
     public static long getLatestOpeningMilis() {
